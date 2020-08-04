59c6a3268a911c93854330ce27bebb5abfdcb103
shen_nicholas <shen_nicholas@163.com>
2020-08-04 17:15:25 +0800

    存在问题：显示倒序

--- main.js
+++ main.js
@@ -1,21 +1,51 @@
 import {ToyReact, Component} from "./ToyReact.js";
 
-class MyComponent extends Component {
+class Square extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value : null
+        }
+    }
     render() {
-        return <div>
-            <span>Hello</span>
-            <span>World!</span>
+        return (
+            <button class="square" onClick={() => this.setState({value: 'X'})}>
+                {this.props.value >= 0 ? this.props.value : ""}
+            </button>
+        );
+    }
+}
+
+class Board extends Component {
+    renderSquare(i) {
+        console.log(i);
+        return <Square value={i}></Square>
+    }
+    render() {
+        return (
             <div>
-                {this.children}
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
             </div>
-        </div>;
-    };
+        )
+    }
 }
 
 // let a = <MyComponent name="a" />;
-let a = <MyComponent name="a" id="ida">
-    <div>123</div>
-</MyComponent>
+let a = <Board />
 
 ToyReact.render(
     a,
