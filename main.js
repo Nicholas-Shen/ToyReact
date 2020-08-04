9c910138d9fbef150733e6b115989ffdc205e2f8
shen_nicholas <shen_nicholas@163.com>
2020-08-04 12:30:50 +0800

    ToyReact Day1 homework

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
-import {ToyReact} from "./ToyReact.js";
+import {ToyReact, Component} from "./ToyReact.js";
 
-class MyComponent {
-
+class MyComponent extends Component {
+    render() {
+        return <div>
+            <span>Hello</span>
+            <span>World!</span>
+            <div>
+                {this.children}
+            </div>
+        </div>;
+    };
 }
 
 // let a = <MyComponent name="a" />;
-let a = <div name="a" />;
+let a = <MyComponent name="a" id="ida">
+    <div>123</div>
+</MyComponent>
+
+ToyReact.render(
+    a,
+    document.body,
+);
